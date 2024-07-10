4e63b3e00db24c82f0dacd962757a52f3a4349eb
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-10 16:50:11 +0200

    Test function type

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ console.log("Hello World!");
 const any = 69;
 let whatever;
 whatever = false;
+let newFunc;
+const addition = (n1, n2) => {
+    return (n1 + n2).toString();
+};
+newFunc = addition;
+console.log(newFunc(10, 20));
--- index.ts
+++ index.ts
@@ -7,3 +7,13 @@ let whatever: string | boolean;
 whatever = false;
 
 //prova di commento
+
+let newFunc: (x: number, y: number) => string;
+
+const addition = (n1: number, n2: number): string => {
+  return (n1 + n2).toString();
+};
+
+newFunc = addition;
+
+console.log(newFunc(10, 20));
