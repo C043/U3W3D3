09e55d94c73a0d057496d793c413ff880e4f64f6
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-10 16:41:46 +0200

    Start testing on TypeScript

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello World!");
+const any = 69;
+let whatever;
+whatever = false;
---
+++ index.ts
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+
+const any: number = 69;
+
+let whatever: string | boolean;
+
+whatever = false;
+
+//prova di commento
